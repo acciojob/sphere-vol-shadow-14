d1ea090a9fc1af693f170dd900329a5665803db1
Sarthak Goyal <82680055+shadow-14@users.noreply.github.com>
2025-06-15 08:44:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-function volume_sphere() {
+function isPositiveNumber(str) {
+    let num = Number(str);
+    return !isNaN(num) && num >= 0;
+}
+
+function volume_sphere(event) {
     //Write your code here
-  
+	 event.preventDefault(); 
+	let ans = NaN;
+	let radius =document.getElementById('MyForm')["radius"].value;
+  if(isPositiveNumber(radius)){
+	  ans = (4 / 3) * Math.PI * Math.pow(radius, 3);
+  }
+	let b = document.getElementById('MyForm')["volume"];
+	b.value = ans;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
